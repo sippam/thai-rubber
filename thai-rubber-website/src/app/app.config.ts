028c1cdc336c67be7b24cc891028bfc4b603d7e1
sippam <sippakorn.wo@kkumail.com>
2024-12-24 03:57:51 +0700

    [Add] something in app.config

--- thai-rubber-website/src/app/app.config.ts
+++ thai-rubber-website/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -7,7 +7,8 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    // provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })), // เพิ่ม Scroll Restoration
     provideAnimationsAsync(),
     provideAnimationsAsync(),
     provideAnimationsAsync(),
