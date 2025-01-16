da5d28abc94863cb98e19184deb65bd0029cec25
sippam <sippakorn.wo@kkumail.com>
2025-01-16 16:18:02 +0700

    [Add] n p k

--- thai-rubber-backend/src/routes/npk.ts
+++ thai-rubber-backend/src/routes/npk.ts
@@ -1,10 +1,20 @@
 import { Router, Request, Response } from "express";
+import pool from "../config/database";
 
 const router: Router = Router();
 
-router.post("/v1/soil/post", (req, res) => {
-    console.log(req.body)
-    res.status(200).json({ message: "success", data: JSON.stringify(req.body)});
-})
+router.post("/v1/soil/post", async (req, res) => {
+  console.log(req.body);
+  const { N, P, K, PH, Humidity } = req.body;
 
-export default router
+  const sql = `
+        INSERT INTO npk (n, p, k, ph, humidity) 
+        VALUES (?, ?, ?, ?, ?)
+      `;
+      
+  await pool.query(sql, [N, P, K, PH, Humidity]);
+
+  res.status(200).json({ message: "success", data: JSON.stringify(req.body) });
+});
+
+export default router;
