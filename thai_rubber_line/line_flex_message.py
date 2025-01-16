eef72fb0799ea6e6b512a01e9b788646974ae1b9
sippam <sippakorn.wo@kkumail.com>
2025-01-16 17:31:11 +0700

    [Add] items in line

--- thai_rubber_line/line_flex_message.py
+++ thai_rubber_line/line_flex_message.py
@@ -14,6 +14,58 @@ channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 
 data_json = {}
 
+items = [
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023273/kaito_crdd00.jpg",
+        "name": "เชื้อราไตรโคเดอร์มา",
+        "price": "195",
+        "market": "shopee / Lazada"
+    },
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023273/white_rtayxe.jpg",
+        "name": "ปูนขาว",
+        "price": "195",
+        "market": "shopee / Lazada / ร้านพันทวี"
+    },
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023274/doromai_ylwo39.png",
+        "name": "โดโลไมท์",
+        "price": "195",
+        "market": "บริษัททีพีไอ โพลีน จํากัด (มหาชน)"
+    },
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023273/yuria_xvzgt8.jpg",
+        "name": "ยูเรีย",
+        "price": "195",
+        "market": "shopee / Lazada"
+    },
+    #   ราแป้ง
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023274/benomyl_klgptd.jpg",
+        "name": "Benomyl 50% WP",
+        "price": "200",
+        "market": "ร้านขายเคมีเกษตร / Shopee / Lazada"
+    },
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023273/carbendazim_otkw2p.jpg",
+        "name": "Carbendazim 50% WP",
+        "price": "299 / ลิตร",
+        "market": "ร้านขายเคมีเกษตร / Shopee / Lazada"
+    },
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023273/cyproconazole_u5ovgy.jpg",
+        "name": "Cyproconazole 10% SL",
+        "price": "235 / 100 cc",
+        "market": "ร้านขายเคมีเกษตร / Shopee / Lazada"
+    },
+    {
+        "url": "https://res.cloudinary.com/djfkjbnnr/image/upload/v1737023273/hexaconazole_q0ac1p.jpg",
+        "name": "Hexaconazole 5% SC",
+        "price": "130 บาท / ลิตร ",
+        "market": "ร้านขายเคมีเกษตร / Shopee / Lazada"
+    },
+]
+
 bubble_template = """
 {
     "type": "bubble",
@@ -51,14 +103,59 @@ bubble_template = """
 }
 """
 
+items_template = """
+{
+    "type": "bubble",
+    "hero": {
+        "type": "image",
+        "url": "%s",
+        "size": "md",
+        "aspectRatio": "20:13",
+        "aspectMode": "fit",
+        "action": {
+            "type": "uri",
+            "uri": "https://line.me/"
+        }
+    },
+    "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": "%s",
+                "weight": "bold",
+                "size": "xl"
+            },
+            {
+                "type": "text",
+                "text": "ราคา %s บาท",
+                "size": "md"
+            },
+            {
+                "type": "text",
+                "text": "ซื้อได้ที่ %s",
+                "size": "md"
+            }
+        ]
+    }
+}
+"""
+
+
 def flex_message_function(uid, items_array, predicted_class):
 
     with open('test.json', 'r', encoding='utf-8') as file:
         data_json = json.load(file)
-        
+
     bubbles = [
-        json.loads(bubble_template % (item['img_url'], item['header'], item['action']))  # Replace placeholders
-        for item in items_array
+        # Replace placeholders
+        json.loads(bubble_template % (item['img_url'], item['header'], item['action'])) for item in items_array
+    ]
+
+    bubbles_items = [
+        # Replace placeholders
+        json.loads(items_template % (item['url'], item['name'], item['price'], item['market'])) for item in items
     ]
 
     user_id = uid
@@ -80,7 +177,33 @@ def flex_message_function(uid, items_array, predicted_class):
 
     headers = {
         'Content-Type': 'application/json',
-        'Authorization': f'Bearer {channel_access_token}'  # Use Bearer Authorization
+        # Use Bearer Authorization
+        'Authorization': f'Bearer {channel_access_token}'
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(flex_message))
+    response = requests.post(url, headers=headers,
+                             data=json.dumps(flex_message))
+
+    # Final Flex Message template
+    flex_message = {
+        "to": user_id,
+        "messages": [
+            {
+                "type": "flex",
+                "altText": "ข้อมูลโรค",
+                "contents": {
+                    "type": "carousel",
+                    "contents": bubbles_items
+                }
+            }
+        ]
+    }
+
+    headers = {
+        'Content-Type': 'application/json',
+        # Use Bearer Authorization
+        'Authorization': f'Bearer {channel_access_token}'
+    }
+
+    response = requests.post(url, headers=headers,
+                             data=json.dumps(flex_message))
